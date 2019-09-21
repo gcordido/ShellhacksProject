6ee1f44db8327dd4a86dc22ae4dbf12f2c7037ce
apast <apast005@fiu.edu>
2019-09-21 17:36:22 -0400

    Initial login page setup, proof of concept

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AngularFireAuth} from "@angular/fire/auth";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+
+  loginForm = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
+  });
+
+  constructor(private fbAuth: AngularFireAuth) { }
+
+  onLogin() {
+
+    const email = this.loginForm.value.email;
+    const password = this.loginForm.value.password;
+
+    this.fbAuth.auth.signInWithEmailAndPassword(email, password)
+      .then(response => {
+        console.log(response);
+        console.log("Successfully logged in");
+      })
+      .catch(error => {
+        console.error(error);
+      });
+
+  }
+
+}
